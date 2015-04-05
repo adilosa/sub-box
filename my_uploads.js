2a1949d2edcee7f1c7deb9373a20ccbe01f1a791
Andrew DiLosa <andrew@dilosa.com>
2015-04-05 15:09:33 -0700

    Changed to show latest 5 videos from all subscribed channels, sorted by publishedAt date. It seems publishedAt gives date added to the "Uploads" playlist, which doesn't seem to correspond to when the video went public.

--- my_uploads.js
+++ my_uploads.js
@@ -1,71 +1,87 @@
-// Define some variables used to remember state.
-var playlistId, nextPageToken, prevPageToken;
+var videos = [];
 
-// After the API loads, call a function to get the uploads playlist ID.
 function handleAPILoaded() {
-  requestUserUploadsPlaylistId();
+    requestUserSubscriptions();
 }
 
-// Call the Data API to retrieve the playlist ID that uniquely identifies the
-// list of videos uploaded to the currently authenticated user's channel.
-function requestUserUploadsPlaylistId() {
-  // See https://developers.google.com/youtube/v3/docs/channels/list
-  var request = gapi.client.youtube.channels.list({
-    mine: true,
-    part: 'contentDetails'
-  });
-  request.execute(function(response) {
-    playlistId = response.result.items[0].contentDetails.relatedPlaylists.uploads;
-    requestVideoPlaylist(playlistId);
-  });
+function requestUserSubscriptions(pageToken) {
+    var requestOptions = {
+        mine: true,
+        part: 'snippet',
+        maxResults: 50
+    }
+    if (pageToken) {
+        requestOptions.pageToken = pageToken;
+    }
+    var request = gapi.client.youtube.subscriptions.list(requestOptions);
+    request.execute(function (response) {
+        var subscriptions = response.result.items;
+        if (subscriptions) {
+            $.each(subscriptions, function(i, subscription) {
+                requestChannelInfo(subscription.snippet.resourceId.channelId);
+            });
+        } else {
+            $("#video-container").html('Sorry you have no subscriptions');
+        }
+        if (response.nextPageToken) {
+            requestUserSubscriptions(response.nextPageToken);
+        }
+    });
+}
+
+function requestChannelInfo(channelId) {
+    var request = gapi.client.youtube.channels.list({
+        part: 'contentDetails',
+        id: channelId
+    });
+    request.execute(function (response) {
+        var channel = response.result.items;
+        if (channel) {
+            var uploadsId = channel[0].contentDetails.relatedPlaylists.uploads;
+            requestVideoPlaylist(uploadsId);
+        } else {
+            alert("No such channel " + channelId);
+        }
+    });
 }
 
 // Retrieve the list of videos in the specified playlist.
-function requestVideoPlaylist(playlistId, pageToken) {
-  $('#video-container').html('');
-  var requestOptions = {
-    playlistId: playlistId,
-    part: 'snippet',
-    maxResults: 10
-  };
-  if (pageToken) {
-    requestOptions.pageToken = pageToken;
-  }
-  var request = gapi.client.youtube.playlistItems.list(requestOptions);
-  request.execute(function(response) {
-    // Only show pagination buttons if there is a pagination token for the
-    // next or previous page of results.
-    nextPageToken = response.result.nextPageToken;
-    var nextVis = nextPageToken ? 'visible' : 'hidden';
-    $('#next-button').css('visibility', nextVis);
-    prevPageToken = response.result.prevPageToken
-    var prevVis = prevPageToken ? 'visible' : 'hidden';
-    $('#prev-button').css('visibility', prevVis);
-
-    var playlistItems = response.result.items;
-    if (playlistItems) {
-      $.each(playlistItems, function(index, item) {
-        displayResult(item.snippet);
-      });
-    } else {
-      $('#video-container').html('Sorry you have no uploaded videos');
-    }
-  });
+function requestVideoPlaylist(playlistId) {
+    var request = gapi.client.youtube.playlistItems.list({
+        playlistId: playlistId,
+        part: 'snippet',
+        maxResults: 5
+    });
+    request.execute(function (response) {
+        var playlistItems = response.result.items;
+        if (playlistItems) {
+            $.each(playlistItems, function (index, item) {
+                videos.push(item.snippet);
+            });
+        } else {
+            $('#video-container').html('Sorry you have no uploaded videos');
+        }
+    });
 }
 
-// Create a listing for a video.
+function displayVideos() {
+    console.log(videos.length);
+    videos.sort(PlaylistItemComparator);
+    var html = "";
+    $.each(videos, function(i, video) {
+        html += "<p>" + video.title + " - " + video.channelTitle + " - " + new Date(video.publishedAt).toLocaleString() + "</p>";
+    });
+    $("#video-container").html(html);
+}
+
+function PlaylistItemComparator(a, b) {
+    var aDate = new Date(a.publishedAt);
+    var bDate = new Date(b.publishedAt);
+    return aDate > bDate ? -1 : (aDate < bDate ? 1 : 0);
+}
+
 function displayResult(videoSnippet) {
-  var title = videoSnippet.title;
-  var videoId = videoSnippet.resourceId.videoId;
-  $('#video-container').append('<p>' + title + ' - ' + videoId + '</p>');
-}
-
-// Retrieve the next page of videos in the playlist.
-function nextPage() {
-  requestVideoPlaylist(playlistId, nextPageToken);
-}
-
-// Retrieve the previous page of videos in the playlist.
-function previousPage() {
-  requestVideoPlaylist(playlistId, prevPageToken);
+    var title = videoSnippet.title;
+    var videoId = videoSnippet.resourceId.videoId;
+    $('#video-container').append('<p>' + title + ' - ' + videoSnippet.channelTitle + '</p>');
 }
